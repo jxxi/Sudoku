0c696aeb2ce2ef69d3e34cecfe155d37ded4bb82
Jasmine Wiggins <jasminerwiggins@gmail.com>
2015-03-21 21:37:43 -0500

    Updated tests and sudoku board implementation

--- src/sudoku/Sudoku.groovy
+++ src/sudoku/Sudoku.groovy
@@ -1,4 +1,23 @@
 package sudoku
 
 class Sudoku {
+    def sudokuBoard
+
+    Sudoku(){
+        sudokuBoard = new Object[9][9]
+    }
+
+    def placeNumber(def entry, def row, def column){
+
+        checkEntry(entry, row, column)
+        sudokuBoard[row][column] = entry
+    }
+
+    def checkEntry(def entry, def row, def column){
+        if(entry instanceof String || entry < 0 || entry > 9)
+            throw new IllegalArgumentException()
+
+        else if(row < 0 || row > 9 || column < 0 || column > 9)
+            throw new ArrayIndexOutOfBoundsException()
+    }
 }
--- test/sudoku/SudokuTest.groovy
+++ test/sudoku/SudokuTest.groovy
@@ -2,15 +2,54 @@ package sudoku
 
 import spock.lang.*
 
-
 class SudokuTest extends Specification{
     Sudoku sudoku
+    def sudokuBoard
 
     def setup(){
         sudoku = new Sudoku()
+        sudokuBoard  = new Object[9][9]
     }
 
-    def "placeNumber places a valid number at the specified position"(){
-
+    def "canary test"(){
+        expect:
+        true
     }
+
+    def "placeNumber places a valid number at a valid specified position"(){
+        sudoku.placeNumber(1, 0, 0)
+        expect:
+        sudoku.sudokuBoard[0][0] == 1
+    }
+
+    def "checkEntry throws IllegalArgumentException if entry is not between 0 and 9 and the entry is not placed on board"(){
+        when:
+        sudoku.checkEntry(-1, 0, 0)
+        then:
+        thrown IllegalArgumentException
+        sudoku.sudokuBoard[0][0] != -1
+    }
+
+    def "checkEntry throws IllegalArgumentException if entry is not a number and the entry is not placed on board"(){
+        when:
+        sudoku.checkEntry("a", 0, 0)
+        then:
+        thrown IllegalArgumentException
+        sudoku.sudokuBoard[0][0] != "a"
+    }
+
+    def "checkEntry throws OutOfBoundsException if row is out of bounds"(){
+        when:
+        sudoku.checkEntry(1, 10, 0)
+        then:
+        thrown ArrayIndexOutOfBoundsException
+    }
+
+    def "checkEntry throws OutOfBoundsException if column is out of bounds"(){
+        when:
+        sudoku.checkEntry(1, 0, 10)
+        then:
+        thrown ArrayIndexOutOfBoundsException
+    }
+
 }
