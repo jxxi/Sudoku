069900e4305b06233047b0172cb2f0a8f5722573
Jasmine Wiggins <jasminerwiggins@gmail.com>
2015-03-21 20:17:43 -0500

    Created Sudoku class and tests

---
+++ src/sudoku/Sudoku.groovy
@@ -0,0 +1,4 @@
+package sudoku
+
+class Sudoku {
+}
---
+++ test/sudoku/SudokuTest.groovy
@@ -0,0 +1,16 @@
+package sudoku
+
+import spock.lang.*
+
+
+class SudokuTest extends Specification{
+    Sudoku sudoku
+
+    def setup(){
+        sudoku = new Sudoku()
+    }
+
+    def "placeNumber places a valid number at the specified position"(){
+
+    }
+}
